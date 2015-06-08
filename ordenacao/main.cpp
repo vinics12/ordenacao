e3e0dc580ae57aa28c2f1eba557707a31e8493b0
Vinícius Cerqueira Silva <vini.cerqueira12@gmail.com>
2015-06-07 21:30:27 -0400

    Initial Commit

---
+++ ordenacao/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ordenacao
+//
+//  Created by Vinícius Cerqueira Silva on 07/06/15.
+//  Copyright (c) 2015 BEPiD FUCAPI. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
